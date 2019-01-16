ebcd329786b405dd0c9cc32e8bd068ffdb0f7a0e
thedevscott <30559808+thedevscott@users.noreply.github.com>
2019-01-16 16:40:31 -0500

    Text file searcher app with recursion completed

---
+++ file_searcher/README.md
@@ -0,0 +1,10 @@
+# File Searcher App
+
+Application to search a large amount f files for specific text
+
+## Topics
+* yield/yield from
+* Recursion
+* Path operations
+* Generator methods
+* String searching
--- file_searcher/app.py
+++ file_searcher/app.py
@@ -1,1 +1,81 @@
 """File Searcher"""
+import collections
+import os
+
+from tp_common import print_header
+
+SearchResult = collections.namedtuple('SearchResult',
+                                      'file, line, text')
+
+
+def main():
+    print_header('File Searcher')
+    folder = get_folder_from_user()
+    if not folder:
+        print("Sorry, we can't search that location")
+        return
+
+    text = get_search_text_from_user()
+    if not text:
+        print("Sorry we cant search for nothing!")
+        return
+
+    matches = search_folders(folder, text)
+
+    if not matches:
+        print('No match found')
+    else:
+        for m in matches:
+            print('-------Match-------')
+            print("file: {}\nline: {}\nmatch: {}".format(
+                m.file, m.line, m.text.strip()))
+
+
+def get_folder_from_user():
+    folder = input('What folder do you want to search? ')
+    if not folder or not folder.strip():
+        return None
+
+    if not os.path.isdir(folder):
+        return None
+
+    return os.path.abspath(folder)
+
+
+def get_search_text_from_user():
+    text = input('What are you searching for [single phrases only]? ')
+    return text.lower()
+
+
+def search_file(filename, search_text):
+    matches = []
+    with open(filename, 'r', encoding='utf-8') as fin:
+
+        line_num = 0
+        for line in fin:
+            line_num += 1
+            if line.lower().find(search_text) >= 0:
+                m = SearchResult(text=line, file=filename, line=line_num)
+                matches.append(m)
+
+        return matches
+
+
+def search_folders(folder, text):
+    all_matches = []
+    items = os.listdir(folder)
+
+    for item in items:
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(full_item):
+            matches = search_folders(full_item, text)
+        else:
+            matches = search_file(full_item, text)
+
+        all_matches.extend(matches)
+
+    return all_matches
+
+
+if __name__ == '__main__':
+    main()
