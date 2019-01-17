edc93b220e097783f1be65825de46fdefa3b8bb3
thedevscott <30559808+thedevscott@users.noreply.github.com>
2019-01-17 09:23:14 -0500

    Basic search works. Needs to be cleaned up

--- movie_search/app.py
+++ movie_search/app.py
@@ -1,3 +1,33 @@
 """Movie Search"""
+import requests
+import collections
 
 from tp_common import print_header
+
+
+def run_event_loop():
+    while True:
+
+        search = input("What would you like to search for? E[x]it: ")
+
+        if search.lower() == 'x':
+            print('Bye')
+            break
+
+        url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
+
+        resp = requests.get(url)
+        resp.raise_for_status()
+        movie_data = resp.json()
+        movies = movie_data.get('hits')
+
+        print(movies)
+
+
+def main():
+    print_header("Movie Search")
+    run_event_loop()
+
+
+if __name__ == '__main__':
+    main()
